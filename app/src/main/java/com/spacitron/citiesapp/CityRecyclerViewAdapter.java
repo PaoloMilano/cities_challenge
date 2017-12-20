900e9c581d0e2802f79d62ad4c55d27717caf740
Paolo Milano <spacitron999@gmail.com>
2017-12-20 22:43:15 +0100

    List of cities can now be displayed.

---
+++ app/src/main/java/com/spacitron/citiesapp/CityRecyclerViewAdapter.java
@@ -0,0 +1,58 @@
+package com.spacitron.citiesapp;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.spacitron.citiesapp.databinding.CityItemBinding;
+
+import java.util.List;
+
+
+public class CityRecyclerViewAdapter extends RecyclerView.Adapter<CityRecyclerViewAdapter.CityViewHolder> {
+
+    final List<CityViewModel.FilterableCity> cities;
+
+    public CityRecyclerViewAdapter(final List<CityViewModel.FilterableCity> cities) {
+        this.cities = cities;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public CityRecyclerViewAdapter.CityViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new CityViewHolder(DataBindingUtil.<CityItemBinding>inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.city_item,
+                parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(CityViewHolder holder, int position) {
+        holder.bind(cities.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cities.size();
+    }
+
+    public class CityViewHolder extends RecyclerView.ViewHolder {
+
+        private final CityItemBinding binding;
+
+        public CityViewHolder(CityItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(CityViewModel.FilterableCity cityItem) {
+            binding.setCity(cityItem);
+            binding.executePendingBindings();
+        }
+    }
+}
--- app/src/main/java/com/spacitron/citiesapp/MainActivity.java
+++ app/src/main/java/com/spacitron/citiesapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.spacitron.citiesapp;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.databinding.ObservableArrayList;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.spacitron.citiesapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CityViewModel cityViewModel = ViewModelProviders.of(this).get(CityViewModel.class);
+        cityViewModel.init(this);
+
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setCityViewModel(cityViewModel);
+
+    }
+
+    @BindingAdapter("bind:items")
+    public static void bindList(RecyclerView view, ObservableArrayList<CityViewModel.FilterableCity> list) {
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+        view.setLayoutManager(layoutManager);
+        view.setAdapter(new CityRecyclerViewAdapter(list));
     }
 }
